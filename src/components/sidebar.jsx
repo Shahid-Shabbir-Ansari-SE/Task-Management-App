3cc5745139d9ff3143cbba53fe210b2786efb638
Shahid-Shabbir-Ansari-SE <shahid.202202646@gcuf.edu.pk>
2023-09-20 17:50:03 +0500

    commit

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -27,6 +27,19 @@ const sidebar = ({ darkMode, toggleDarkMode }) => {
       <h1 className="text-[21px] font-Poppins mx -3 text-gray-600 dark:text-white">
         OrganizeMeNow
       </h1>
+      <div className="flex gap-3 items-center justify-center lg:pt-5 w-full lg:hidden md:hidden">
+            <BsSunFill className="text-2xl dark:text-white" />
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                onChange={toggleDarkMode}
+                type="checkbox"
+                checked={darkMode}
+                className="sr-only peer"
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <BsMoonStarsFill className="ml-3 text-xl dark:text-white" />
+            </label>
+          </div>
       </div>
       <aside
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
